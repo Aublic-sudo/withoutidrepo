0337d3904e6572eb3ea7fd2b332bcf451f450c32
Aublic-sudo <rstock937@gmail.com>
2026-03-15 11:06:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,32 +1,37 @@
 import os
 
 class Config(object):
-    # Telegram Bot ka token
-    BOT_TOKEN = "7678043069:AAGNFEnxKftkMd8alQuT-6AwRP0LmRcEgPw"
-    # Telegram API ki ID
-    API_ID = 25513009
-    # Telegram API ki hash key
-    API_HASH = "33ace9b0779df4387a8ad8b7fbe6b050"
-    ADMIN_ID = [8130693100]
-    # MongoDB database ka URL
-    DB_URL = "mongodb+srv://sumersingh5688977:TGZmTpS3E803laT4@cluster0.4cikn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    # Database ka naam
-    DB_NAME = "MY_BOT_DB"
-    # Text log channel ki ID
-    TXT_LOG = -1002504285151
-    # Authentication log channel ki ID
-    AUTH_LOG = -1002504285151
-    # Hit log channel ki ID
-    HIT_LOG = -1002504285151
-    # DRM dump channel ki ID
-    DRM_DUMP = -1002504285151
-    # Main channel ki ID
-    CHANNEL = -1002504285151
-    # Channel ka link
-    CH_URL = "https://t.me/+21UeX7WNeIoyM2U1"
-    # Bot ke owner ka Telegram link
-    OWNER = "https://t.me/Rolexonee"
-    # Thumbnail image ka URL
-    THUMB_URL = "https://telegra.ph/file/example-thumb-image.jpg" #Replace by with your Thumb URL
-    # API host ka URL
-    HOST = "https://api.masterapi.tech"
+    # Telegram Bot token
+    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+
+    # Telegram API credentials
+    API_ID = int(os.environ.get("API_ID", 0))
+    API_HASH = os.environ.get("API_HASH")
+
+    # Admin ID
+    ADMIN_ID = int(os.environ.get("ADMIN_ID", 0))
+
+    # MongoDB database URL
+    DB_URL = os.environ.get("DB_URL")
+
+    # Database name
+    DB_NAME = os.environ.get("DB_NAME", "Cluster0")
+
+    # Log channels
+    TXT_LOG = int(os.environ.get("TXT_LOG", 0))
+    AUTH_LOG = int(os.environ.get("AUTH_LOG", 0))
+    HIT_LOG = int(os.environ.get("HIT_LOG", 0))
+    DRM_DUMP = int(os.environ.get("DRM_DUMP", 0))
+
+    # Main channel
+    CHANNEL = int(os.environ.get("CHANNEL", 0))
+
+    # Links
+    CH_URL = os.environ.get("CH_URL")
+    OWNER = os.environ.get("OWNER")
+
+    # Thumbnail
+    THUMB_URL = os.environ.get("THUMB_URL")
+
+    # API host
+    HOST = os.environ.get("HOST")
